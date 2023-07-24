1f37a9de205a27a8d085c9f1a50cd5affdee8521
Clem <cineaves6@hotmail.com>
2023-07-24 21:44:19 +1000

    added project and firebase

---
+++ src/config/Config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC5QE3cTO8VCmQ5-NuQyXcnlXMyVKe-wJc",
+    authDomain: "classassessment-e407e.firebaseapp.com",
+    projectId: "classassessment-e407e",
+    storageBucket: "classassessment-e407e.appspot.com",
+    messagingSenderId: "1038299145067",
+    appId: "1:1038299145067:web:7c8109c885b77788e4622e"
+  };
