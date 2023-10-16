15d286d83baeb3d0f510babb4e7a118f55367fad
Clem <cineaves6@hotmail.com>
2023-10-16 22:56:53 +1100

    added margin to columns and change to 4

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -51,7 +51,7 @@ export function Home () {
 
     const Columns = data.map( (movie, key) => {
         return(
-            <Col md="4" key={key}>
+            <Col md="3" key={key} className="my-3">
                 <Card>
                     <Image path={movie.image} />
                     <Card.Body>
